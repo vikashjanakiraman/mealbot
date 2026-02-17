265a9b84ffb1510201d4b131e6d95dd8e14ae641
Vikash Janakiraman <vikashjanakiraman@Vikashs-Mac-mini.local>
2026-02-17 13:57:32 +0530

    fix: update integer to biginteger in import

--- app/models/food_log.py
+++ app/models/food_log.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, BigInteger, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database.base import Base
